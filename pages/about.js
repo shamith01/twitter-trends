f23ff5afba320d78c9250ab665e3b76d17e62631
shamith01 <128206892+shamith01@users.noreply.github.com>
2023-03-29 01:09:24 -0700

    Add files via upload

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+
+function about() {
+  return (<>
+  <Navbar/>
+    <div >
+      this is about page
+    </div>
+  </>
+  )
+}
+
+export default about
